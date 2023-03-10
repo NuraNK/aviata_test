4abc842d2dddf0f584fe6d349117f566686c764e
nurank <abdilkakovn@gmail.com>
2023-03-10 13:59:22 +0600

    123

--- airflow/tasks.py
+++ airflow/tasks.py
@@ -4,5 +4,5 @@ from parse_xml import parse_xml_currency
 
 async def currency_cron_task():
     redis = await aioredis.from_url("redis://redis:6379")
-    await redis.set('currency', parse_xml_currency(), ex=60 * 60 * 24, nx=True)
+    await redis.set('currency', parse_xml_currency(), ex=60 * 60 * 24)
     await redis.close()
